f3e4d5cfd6959fb34d802204d0d613605444dd2e
jourzero <epaquet@192.168.1.100>
2016-10-11 10:25:56 -0400

    Minor update

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ _Context: Proxy/HTTP-History_
 * Supports the selection of multiple HTTP messages
 
 _Context: Target/Issues_
-* Add Menu "Copy as free text" that puts the issue details in free text form into the clipboard
+* Add Menu "Copy as free text to clipboard" that puts the issue details in free text form into the clipboard
 * Supports the selection of multiple issues
 * When an issue has multiple instances, all URLs are listed together
 * Only the first HTTP Request/Response pair is included as Evidence data (Base64-encoded).
--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,5 +1,5 @@
 /*
- * Burpaholic extension
+ * Clipboarder extension
  */
 package burp;
 
@@ -14,7 +14,7 @@ import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.List;
 import javax.swing.JMenuItem;
-import java.util.Base64.Encoder;
+import java.util.Base64;
 
 public class BurpExtender implements IBurpExtender, IContextMenuFactory, ClipboardOwner {
     private IExtensionHelpers helpers;
@@ -33,87 +33,127 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, Clipboa
         this.helpers = callbacks.getHelpers();
         callbacks.setExtensionName(EXT_NAME);
         callbacks.registerContextMenuFactory((IContextMenuFactory)this);
-        this.stdout.println("Use " + EXT_NAME+" to quickly extract data from Burp to feed other tools or reports:"
+        this.stdout.println(
+                  "\n======================================================================================"
+                + "\nUse " + EXT_NAME+" to quickly extract data from Burp to feed other tools or reports:"
                 + "\n- In Proxy/History, choose " + PROXYHIST_CONTEXTMENU_COPYRAW 
                 + "\n- In Target/Issues, choose " + TARGETISSUES_CONTEXTMENU_COPYTEXT
-                + "\nCheers,\nEric Paquet <eric@jourzero.com>\n");
+                + "\nLatest source here: https://github.com/jourzero/clipboarder"
+                + "\nCheers,"
+                + "\nEric Paquet <eric@jourzero.com>"
+                + "\n======================================================================================\n");
     }
 
+    /**
+     * Create Burp menu items
+     * @param invocation
+     * @return List of menu items to create in Burp context
+     */
     public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
         final IHttpRequestResponse[] messages = invocation.getSelectedMessages();
-        if (messages != null && messages.length != 0) {
-            JMenuItem i = new JMenuItem(PROXYHIST_CONTEXTMENU_COPYRAW);
-            i.addActionListener(new ActionListener(){
+        try{
+            if (messages != null && messages.length != 0) {
+                JMenuItem i = new JMenuItem(PROXYHIST_CONTEXTMENU_COPYRAW);
+                i.addActionListener(new ActionListener(){
 
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    BurpExtender.this.copyRawHttp(messages);
-                }
-            });
-            return Collections.singletonList(i);
-        }
-        final IScanIssue[] issues = invocation.getSelectedIssues();
-        if (issues != null && issues.length != 0) {
-            JMenuItem i = new JMenuItem(TARGETISSUES_CONTEXTMENU_COPYTEXT);
-            i.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        BurpExtender.this.copyRawHttp(messages);
+                    }
+                });
+                return Collections.singletonList(i);
+            }
+            final IScanIssue[] issues = invocation.getSelectedIssues();
+            if (issues != null && issues.length != 0) {
+                JMenuItem i = new JMenuItem(TARGETISSUES_CONTEXTMENU_COPYTEXT);
+                i.addActionListener(new ActionListener(){
 
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    BurpExtender.this.copyIssueText(issues);
-                }
-            });
-            return Collections.singletonList(i);
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        BurpExtender.this.copyIssueText(issues);
+                    }
+                });
+                return Collections.singletonList(i);
+            }
+            else{
+                return null;
+            }
         }
-        else{
-            return null;
-        }
+        catch (Exception e) {
+            this.stderr.println("ERROR: Failed creating menus for Clipboarder!");
+            e.printStackTrace(this.stderr);
+        }      
+        return null;
     }
     
+    /**
+     * Copy select Burp scan issues as plain text to the clipboard
+     * @param issues List of selected issues in the Target tab
+     */
     private void copyIssueText(IScanIssue[] issues) {
         StringBuilder buf = new StringBuilder();
-        int issueType=0, prevIssueType=0;
-        Boolean firstInstance = true;
+        String issueName="", prevIssueName="", url="", prevUrl="";
         IHttpRequestResponse firstMsg = null;
         strBuf = null;
         
+        // Iterate through all selected issues
         for (IScanIssue issue : issues) {
-            issueType = issue.getIssueType();
-            if (issueType != prevIssueType){
-                firstInstance = true;
+            issueName = issue.getIssueName();
+            this.stdout.println("Reading data for issue: " + issueName);
+            
+            // If the issue name at hand is different, print the details.
+            // If it is not different (from the previous), just print the URL. 
+            // This logic essentially collapses multiple instances of the same issue.
+            if (!issueName.equals(prevIssueName)){
+                this.stdout.println("Copying general issue data for: " + issue.getIssueName());
                 firstMsg = null;
                 strBuf = null;
-                buf.append("----------------------------------------------------------------");
+                buf.append("\n\n----------------------------------------------------------------");
                 buf.append("\nIssue: "                      + issue.getIssueName());
-                buf.append("\nIssue Type: "                 + issueType);
                 buf.append("\nSeverity: "                   + issue.getSeverity());
-                buf.append("\nConfidence:  "                + issue.getConfidence());
+                buf.append("\nConfidence: "                 + issue.getConfidence());
                 buf.append("\nIssue Background:\n"          + issue.getIssueBackground());
-                buf.append("\n~\nIssue Details:\n"          + issue.getIssueDetail());
+                //buf.append("\n~\nIssue Details:\n"          + issue.getIssueDetail());
                 buf.append("\n~\nRemediation Background:\n" + issue.getRemediationBackground());
-                buf.append("\n~\nRemediation Details:\n"    + issue.getRemediationDetail());
-                buf.append("\n~\nEvidence Data (Base64): ");     
-                firstMsg = issue.getHttpMessages()[0];
-                strBuf = new StringBuilder();
-                this.buildRawHttpBuffer(firstMsg);
-                // Add Evidence data
-                if (strBuf != null){
-                    buf.append(this.toBase64(strBuf.toString()));
+                //buf.append("\n~\nRemediation Details:\n"    + issue.getRemediationDetail());
+                
+                // Add evidence data if applicable
+                if (issue.getHttpMessages().length > 0){
+                    buf.append("\n~\nEvidence data (Base64-encoded): ");
+                    strBuf = new StringBuilder();
+                    for (IHttpRequestResponse msg: issue.getHttpMessages()){
+                        this.buildRawHttpBuffer(msg);
+                    }
+                    if (strBuf != null){
+                        buf.append(toBase64(strBuf.toString()));
+                    }
                 }
                 buf.append("\n~\nAffected URL(s):");
             }
-            else
-                firstInstance = false;
             
             // Add URLs that are affected by the issue
-            buf.append("\n - " + issue.getUrl().toString());
-            prevIssueType = issueType;
-            
+            url = issue.getUrl().toString();
+            if (!url.equals(prevUrl)){
+                buf.append("\n - " + url);
+            }
+            prevUrl = url;
+            prevIssueName = issueName;
         }
 
         // Send to clipboard
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(buf.toString()), this);  
+        try{
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(buf.toString()), this);  
+        }
+        catch (Exception e) {
+            this.stderr.println("ERROR: Failed copying issue data to clipboard!");
+            e.printStackTrace(this.stderr);
+        }        
     }
 
+    /**
+     * Copy the raw HTTP text from selected messages in the Burp Proxy History
+     * @param messages selected messages in Burp's proxy history
+     */
     private void copyRawHttp(IHttpRequestResponse[] messages) {
         strBuf = new StringBuilder();
         for (IHttpRequestResponse message : messages) {
@@ -121,9 +161,20 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, Clipboa
         }
 
         // Send to clipboard
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(strBuf.toString()), this);
+        try{
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(strBuf.toString()), this);
+            this.stdout.println("Copied raw HTTP data to clipboard for " + messages.length + " messages");
+        }
+        catch (Exception e) {
+            this.stderr.println("ERROR: Failed copying raw HTTP data to clipboard!");
+            e.printStackTrace(this.stderr);
+        }
     }
 
+    /**
+     * Build the Raw HTTP text representation for a Burp message
+     * @param message Burp HTTP RequestResponse object
+     */
     private void buildRawHttpBuffer(IHttpRequestResponse message){
         
         IRequestInfo ri = this.helpers.analyzeRequest(message);
@@ -134,7 +185,7 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, Clipboa
                 String[] elements = header.split(" ");
                 String url = elements[1];
                 strBuf.append("\n======== PATH: " + url + " ========");
-                strBuf.append("\n*** REQUEST ***\n");
+                strBuf.append("\n===REQUEST===\n");
                 firstHeader = false;
             }
             strBuf.append(header);
@@ -145,21 +196,23 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, Clipboa
             strBuf.append("\n");
             strBuf.append(new String(req, bo, req.length - bo));
         }
+        strBuf.append("\n===\n");
 
         byte[] rsp = message.getResponse();
-        strBuf.append("\n*** RESPONSE ***\n");
+        strBuf.append("\n===RESPONSE===\n");
         if (rsp.length > 0) {
             strBuf.append(this.helpers.bytesToString(rsp));
         }
-        strBuf.append("\n****************");
+        strBuf.append("\n===");
     }
 
-    private String escapeQuotes(String value) {
-        return value.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n").replace("\r", "\\r");
-    }
-    
+    /**
+     * Covert plain text into Base64
+     * @param text Text to encode in Base64
+     * @return Base64-encoded output
+     */
     private String toBase64(String text){        
-        return java.util.Base64.getEncoder().encodeToString(text.getBytes());
+        return Base64.getEncoder().encodeToString(text.getBytes());
     }
 
     @Override
